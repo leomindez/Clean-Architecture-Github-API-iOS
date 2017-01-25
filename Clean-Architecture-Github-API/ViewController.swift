537efb0c91506e3a42a7651771ea1b01ff753c6b
leonel Mendez <Leo Mendez>
2017-01-25 13:32:22 -0600

    Initial Commit

---
+++ Clean-Architecture-Github-API/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clean-Architecture-Github-API
+//
+//  Created by leonel Mendez on 25/01/17.
+//  Copyright © 2017 leonel Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
